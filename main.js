ef0b72de1de7118a23681bc653b86f66bb69a373
Lucy Huang <lucyhuang@lucy-huangs-macbook-4.local>
2017-04-28 16:35:28 -0400

    first draft

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+	$(".filter").on("click", function (e) {
+		e.preventDefault();
+		var button = $(this);
+		var filter = button.data("filter");
+		button.toggleClass("active")
+		if (filter === "reset") {
+			$(".item").addClass("active");
+			$(".filter").addClass("active");
+		} else {
+			$(".item").addClass("active");
+			$(".item.active").removeClass("active");
+			$(".images.active").removeClass("active");
+			$(".filter.active").each(function () {
+				var this_filter = $(this);
+				var filter = this_filter.data("filter");
+				$("." + filter).addClass("active");
+			});
+		}
+	});
+});
