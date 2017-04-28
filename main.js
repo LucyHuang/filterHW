23e25f83a178cd88a31722b6a03b99b5d4c84af6
Brett Dahlberg <brettdahlberg@Bretts-MacBook-Pro.local>
2017-04-28 17:01:46 -0400

    html, css, js changes for appearance.

--- main.js
+++ main.js
@@ -4,13 +4,17 @@ $(document).ready(function () {
 		var button = $(this);
 		var filter = button.data("filter");
 		button.toggleClass("active")
+		// add a class called "active" to everything when we click reset.
 		if (filter === "reset") {
 			$(".item").addClass("active");
 			$(".filter").addClass("active");
+			// $(".resetbutton.active").addClass("active");
+			// when we don't click reset, do something else.
 		} else {
 			$(".item").addClass("active");
 			$(".item.active").removeClass("active");
 			$(".images.active").removeClass("active");
+			$(".resetbutton.active").removeClass("active");
 			$(".filter.active").each(function () {
 				var this_filter = $(this);
 				var filter = this_filter.data("filter");
